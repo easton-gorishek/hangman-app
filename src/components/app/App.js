ce13b781c96fa59ff5077b1fbe5f084d804ac831
Easton Gorishek <ieaston26@gmail.com>
2018-12-29 12:38:28 -0800

    init commit

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+  state = {};
+
+  render() { 
+    return (
+      <h1>Connected</h1>
+    );
+  }
+}
+ 
+export default App;
