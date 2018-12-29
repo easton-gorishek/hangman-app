c3d39e73263c28188764959f1e787c0cc65b2aff
Easton Gorishek <ieaston26@gmail.com>
2018-12-29 13:30:36 -0800

    receiving init response from server

--- src/components/app/Game.js
+++ src/components/app/Game.js
@@ -1,14 +1,29 @@
 import React, { PureComponent } from 'react';
 import Tiles from './Tiles';
+import { startGame } from '../../services/api';
 
 class Game extends PureComponent {
   state = {
-    tiles: ['h', 'a', 'n', 'g', 'm', 'a', 'n']
+    tiles: null
+  };
+
+  init = () => {
+    startGame().then(res => {
+      console.log('RESPONSE', res);
+      this.setState({
+        tiles: res
+      });
+    });
   };
 
   render() { 
     return (
-      <Tiles tiles={this.state.tiles}/>
+      <div>
+        <button onClick={this.init}>Start Game</button>
+        {this.state.tiles &&
+          <Tiles tiles={this.state.tiles}/>
+        }
+      </div>
     );
   }
 }
