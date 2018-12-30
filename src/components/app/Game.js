a98ae11fc76b9d78c9c51b547038c18f2f33e75c
Easton Gorishek <ieaston26@gmail.com>
2018-12-29 16:16:44 -0800

    URL is source of truth

--- src/components/app/Game.js
+++ src/components/app/Game.js
@@ -2,7 +2,7 @@ import React, { PureComponent, Fragment } from 'react';
 import Tiles from './Tiles';
 import Form from './Form';
 import PropTypes from 'prop-types';
-import { startGame, updateGuess } from '../../services/api';
+import { startGame, updateGuess, loadGame } from '../../services/api';
 import qs from 'query-string';
 
 class Game extends PureComponent {
@@ -15,6 +15,22 @@ class Game extends PureComponent {
     history: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired
   };
+
+  componentDidMount() {
+    this.loadGame();
+  }
+
+  componentDidUpdate({ location }) {
+    const { game: oldSearch } = qs.parse(location.search);
+    if(oldSearch === this.game) return;
+    this.loadGame();
+  }
+
+  get game() {
+    const { location } = this.props;
+    const { game } = qs.parse(location.search);
+    return game;
+  }
 
   init = (e) => {
     e.preventDefault();
@@ -39,6 +55,16 @@ class Game extends PureComponent {
           search: qs.stringify({ game: res.key })
         });
       });
+    });
+  };
+
+  loadGame = () => {
+    const { location } = this.props;
+    const game = location.search;
+
+    if(!game) return;
+    loadGame(game).then(res => {
+      this.setState({ ... res });
     });
   };
 
--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,5 @@ const URL = '/api/hangman';
 const START_URL = `${URL}/start-game`;
 
 export const startGame = () => get(START_URL);
+export const loadGame = (game) => get(`${URL}/${game}`);
 export const updateGuess = (guess) => put(URL, guess);
